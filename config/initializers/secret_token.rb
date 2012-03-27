4d15c6d577721677b598c0f9bc4bc60783455de2
Devan Flaherty <devanflaherty@me.com>
2012-03-27 14:25:14 -0400

    Uploading of main application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shorebreak::Application.config.secret_token = '9c9dc9c57a09d70cf5f815ff93fe96d875d2988f6822beae56c05a3a7018711700e47183b0fcbb05c348553c9a1aa0e6e9906475cb974dc39bd4fbe08950737c'
